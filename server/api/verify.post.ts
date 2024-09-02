acd26236748cc050761e2204eef89a7d32885aa5
Muris Ceman <murisceman@gmail.com>
2024-09-03 01:13:20 +0200

    feat: add `rounds`

--- server/api/verify.post.ts
+++ server/api/verify.post.ts
@@ -3,7 +3,9 @@ import { compareSync } from 'bcrypt-edge/dist/bcrypt-edge'
 export default defineEventHandler(async (event) => {
   const { hash, password } = await readBody(event)
 
+  const start = performance.now()
   const isValid = compareSync(password, hash)
+  const end = performance.now()
 
-  return { isValid }
+  return { isValid, time: Math.round(end - start) }
 });
